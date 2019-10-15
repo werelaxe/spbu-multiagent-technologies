b7b121438b738c5375e67c858e568bf1f32a471b
Daniil Sharko <daniil.sharko@jetbrains.com>
2019-10-15 13:25:58 +0300

    Rewrite if with when

--- src/main/kotlin/ReceiveMessage.kt
+++ src/main/kotlin/ReceiveMessage.kt
@@ -10,22 +10,24 @@ class ReceiveMessage(private val agent: DefaultAgent): CyclicBehaviour(agent) {
             agent.prn("Received message: '${message.content}' from ${message.sender.localName}")
             val (rawMsgType, rawValue) = message.content.split(":")
             val value = rawValue.toInt()
-            val msgType = MessageType.valueOf(rawMsgType)
-            if (msgType == MessageType.Request) {
-                agent.parent?.let {
-                    agent.prn("I'm already in progress, Response:0 to ${message.sender.localName}")
-                    agent.sendMessage("Response:0", message.sender.localName)
-                    block()
-                    return
+            when (MessageType.valueOf(rawMsgType)) {
+                MessageType.Request -> {
+                    agent.parent?.let {
+                        agent.prn("I'm already in progress, Response:0 to ${message.sender.localName}")
+                        agent.sendMessage("Response:0", message.sender.localName)
+                        block()
+                        return
+                    }
+                    agent.parent = message.sender.localName
+                    agent.linkedAgents.remove(message.sender.localName)
+                    println("Parent of ${agent.localName} is ${message.sender.localName}")
+                    agent.nextStep()
                 }
-                agent.parent = message.sender.localName
-                agent.linkedAgents.remove(message.sender.localName)
-                println("Parent of ${agent.localName} is ${message.sender.localName}")
-                agent.nextStep()
-            } else {
-                agent.prn("Receive a new value $value from ${message.sender.localName}")
-                agent.result += value
-                agent.nextStep()
+                MessageType.Response -> {
+                    agent.prn("Receive a new value $value from ${message.sender.localName}")
+                    agent.result += value
+                    agent.nextStep()
+                }
             }
             block()
         }
